bbed9e70893870d7c8b0e3626026c0caa314fa84
natalieMillman <natalie@millman.com>
2023-09-28 22:34:07 -0400

    Create sample resources

--- src/main/java/nyc/millman/salesforce/application/SalesforceServiceResourceFactory.java
+++ src/main/java/nyc/millman/salesforce/application/SalesforceServiceResourceFactory.java
@@ -1,25 +1,38 @@
 package nyc.millman.salesforce.application;
 
 import io.dropwizard.core.setup.Environment;
-import nyc.millman.salesforce.resource.MainResource;
+import nyc.millman.salesforce.resource.AccountResource;
+import nyc.millman.salesforce.resource.ContactResource;
+import nyc.millman.salesforce.resource.OpportunityResource;
 import nyc.millman.salesforce.service.SalesforceContext;
 
 public class SalesforceServiceResourceFactory {
 
-    private final SalesforceContext service;
+    private final SalesforceContext context;
     private final Environment environment;
 
-    public SalesforceServiceResourceFactory(Environment environment, SalesforceContext service){
+    public SalesforceServiceResourceFactory(Environment environment, SalesforceContext context){
         this.environment = environment;
-        this.service = service;
+        this.context = context;
     }
 
     public SalesforceServiceResourceFactory registerResources(){
-        environment.jersey().register(getMainResource());
+        environment.jersey().register(getAccountResource());
+        environment.jersey().register(getContactResource());
+        environment.jersey().register(getOpportunityResource());
         return this;
     }
 
-    public MainResource getMainResource(){
-        return new MainResource(service);
+    public AccountResource getAccountResource(){
+        return new AccountResource(context);
     }
+
+    public ContactResource getContactResource(){
+        return new ContactResource(context);
+    }
+
+    public OpportunityResource getOpportunityResource(){
+        return new OpportunityResource(context);
+    }
+
 }
