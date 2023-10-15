b23ced48c82f4ae846184f9e2a1b7f13c0114228
natalieMillman <natalie@millman.com>
2023-10-15 08:07:57 -0500

    Added subscriber

--- src/main/java/nyc/millman/salesforce/application/SalesforceServiceApplication.java
+++ src/main/java/nyc/millman/salesforce/application/SalesforceServiceApplication.java
@@ -3,9 +3,12 @@ package nyc.millman.salesforce.application;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Environment;
 import nyc.millman.salesforce.external.client.SalesforceClient;
+import nyc.millman.salesforce.processor.Subscriber;
 import nyc.millman.salesforce.service.SalesforceContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static nyc.millman.salesforce.processor.PubSubService.printStatusRuntimeException;
 
 public class SalesforceServiceApplication extends Application<SalesforceServiceConfiguration> {
 
@@ -21,6 +24,12 @@ public class SalesforceServiceApplication extends Application<SalesforceServiceC
         SalesforceClient salesforceClient = new SalesforceClient(configuration.getSalesforceClientConfiguration(), configuration.getSalesforceAuthenticationConfiguration());
         SalesforceContext salesforceContext = new SalesforceContext(salesforceClient);
         SalesforceServiceResourceFactory resourceFactory = new SalesforceServiceResourceFactory(environment, salesforceContext).registerResources();
+
+        try (Subscriber subscribe = new Subscriber(configuration.getSubscriberConfiguration(), salesforceClient)) {
+            subscribe.startSubscription();
+        } catch (Exception e) {
+            printStatusRuntimeException("Error during Subscribe", e);
+        }
     }
 
 
