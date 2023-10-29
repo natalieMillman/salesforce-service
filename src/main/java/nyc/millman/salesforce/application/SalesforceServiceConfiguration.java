01f61c17c52b4b0a54bc04bb8b3edc299f267783
natalieMillman <natalie@millman.com>
2023-10-28 23:41:48 -0400

    Added GPRCHostConfiguration

--- src/main/java/nyc/millman/salesforce/application/SalesforceServiceConfiguration.java
+++ src/main/java/nyc/millman/salesforce/application/SalesforceServiceConfiguration.java
@@ -2,6 +2,7 @@ package nyc.millman.salesforce.application;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.core.Configuration;
+import nyc.millman.salesforce.api.configuration.GRPCHostConfiguration;
 import nyc.millman.salesforce.api.configuration.SalesforceAuthenticationConfiguration;
 import nyc.millman.salesforce.api.configuration.SalesforceClientConfiguration;
 import nyc.millman.salesforce.api.configuration.SubscriberConfiguration;
@@ -17,13 +18,18 @@ public class SalesforceServiceConfiguration extends Configuration {
     @JsonProperty("subscriberConfiguration")
     private final SubscriberConfiguration subscriberConfiguration;
 
+    @JsonProperty
+    public final GRPCHostConfiguration grpcConfiguration;
+
     public SalesforceServiceConfiguration(
             @JsonProperty("salesforceClientConfiguration") SalesforceClientConfiguration salesforceClientConfiguration,
             @JsonProperty("salesforceAuthenticationConfiguration") SalesforceAuthenticationConfiguration salesforceAuthenticationConfiguration,
-            @JsonProperty("subscriberConfiguration") SubscriberConfiguration subscriberConfiguration){
+            @JsonProperty("subscriberConfiguration") SubscriberConfiguration subscriberConfiguration,
+            @JsonProperty("grpcConfiguration") GRPCHostConfiguration grpcConfiguration){
         this.salesforceClientConfiguration = salesforceClientConfiguration;
         this.salesforceAuthenticationConfiguration = salesforceAuthenticationConfiguration;
         this.subscriberConfiguration = subscriberConfiguration;
+        this.grpcConfiguration = grpcConfiguration;
     }
 
     @JsonProperty("salesforceClientConfiguration")
@@ -39,6 +45,11 @@ public class SalesforceServiceConfiguration extends Configuration {
     @JsonProperty("subscriberConfiguration")
     public SubscriberConfiguration getSubscriberConfiguration(){
         return subscriberConfiguration;
+    }
+
+    @JsonProperty("grpcConfiguration")
+    public GRPCHostConfiguration getGrpcConfiguration() {
+        return grpcConfiguration;
     }
 
 }
