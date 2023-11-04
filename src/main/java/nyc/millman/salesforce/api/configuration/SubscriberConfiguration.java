cfb520cbae4de35087d6546a13a5dece33615c4f
natalieMillman <natalie@millman.com>
2023-11-04 15:24:13 -0400

    Refactored out PubSubConfiguration object from

--- src/main/java/nyc/millman/salesforce/api/configuration/SubscriberConfiguration.java
+++ src/main/java/nyc/millman/salesforce/api/configuration/SubscriberConfiguration.java
@@ -2,88 +2,31 @@ package nyc.millman.salesforce.api.configuration;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.protobuf.ByteString;
-import com.salesforce.eventbus.protobuf.ReplayPreset;
+import nyc.millman.salesforce.api.Topic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Set;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public final class SubscriberConfiguration {
-    @JsonProperty
-    private final String host;
-    @JsonProperty
-    private final Integer port;
-    @JsonProperty
-    private final String topic;
-    @JsonProperty
-    private final Integer numberOfEventsToSubscribeInEachFetchRequest;
-    @JsonProperty
-    private final Boolean plaintextChannel;
-    @JsonProperty
-    private final ReplayPreset replayPreset;
-    @JsonProperty
-    private final long replayId;
+
+    private final Logger logger = LoggerFactory.getLogger(SubscriberConfiguration.class);
+
+    @JsonProperty("topics")
+    private final Set<Topic> topics;
 
     public SubscriberConfiguration(
-            @JsonProperty("host")  String host,
-            @JsonProperty("port")  Integer port,
-            @JsonProperty("topic")  String topic,
-            @JsonProperty("numberOfEventsToSubscribeInEachFetchRequest")  Integer numberOfEventsToSubscribeInEachFetchRequest,
-            @JsonProperty("plaintextChannel")  Boolean plaintextChannel,
-            @JsonProperty("replayPreset")  ReplayPreset replayPreset,
-            @JsonProperty("replayId") long replayId
-    ) {
-        this.host = host;
-        this.port = port;
-        this.topic = topic;
-        this.numberOfEventsToSubscribeInEachFetchRequest = numberOfEventsToSubscribeInEachFetchRequest;
-        this.plaintextChannel = plaintextChannel;
-        this.replayPreset = replayPreset;
-        this.replayId = replayId;
+            @JsonProperty("topics") Set<Topic> topics) {
+        this.topics = topics;
     }
 
-    @JsonProperty
-    public String getHost() {
-        return host;
-    }
-
-    @JsonProperty
-    public Integer getPort() {
-        return port;
-    }
-
-    @JsonProperty
-    public String getTopic() {
-        return topic;
-    }
-
-    @JsonProperty
-    public Integer getNumberOfEventsToSubscribeInEachFetchRequest() {
-        return numberOfEventsToSubscribeInEachFetchRequest;
-    }
-
-    @JsonProperty
-    public Boolean getPlaintextChannel() {
-        return plaintextChannel;
-    }
-
-    @JsonProperty
-    public ReplayPreset getReplayPreset() {
-        return replayPreset;
-    }
-
-    @JsonProperty
-    public ByteString getReplayId() {
-        return getReplayIdFromLong(replayId);
-    }
-
-    private static ByteString getReplayIdFromLong(long replayValue) {
-        ByteBuffer buffer = ByteBuffer.allocate(8);
-        buffer.putLong(replayValue);
-        buffer.flip();
-
-        return ByteString.copyFrom(buffer);
+    @JsonProperty("topics")
+    public Set<Topic> getTopics() {
+        logger.info("SubscriberConfiguration.getTopics() returning: " + topics);
+        return topics;
     }
 
     @Override
@@ -91,30 +34,17 @@ public final class SubscriberConfiguration {
         if (obj == this) return true;
         if (obj == null || obj.getClass() != this.getClass()) return false;
         var that = (SubscriberConfiguration) obj;
-        return Objects.equals(this.host, that.host) &&
-                Objects.equals(this.port, that.port) &&
-                Objects.equals(this.topic, that.topic) &&
-                Objects.equals(this.numberOfEventsToSubscribeInEachFetchRequest, that.numberOfEventsToSubscribeInEachFetchRequest) &&
-                Objects.equals(this.plaintextChannel, that.plaintextChannel) &&
-                Objects.equals(this.replayPreset, that.replayPreset) &&
-                Objects.equals(this.replayId, that.replayId);
+        return Objects.equals(this.topics, that.topics);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(host, port, topic, numberOfEventsToSubscribeInEachFetchRequest, plaintextChannel, replayPreset, replayId);
+        return Objects.hash(topics);
     }
 
     @Override
     public String toString() {
-        return "\"SubscriberConfiguration\":{" +
-                "\"host\":\"" + host + ":\", " +
-                "\"port\":\"" + port + ":\", " +
-                "\"topic\":\"" + topic + ":\", " +
-                "\"numberOfEventsToSubscribeInEachFetchRequest\":\"" + numberOfEventsToSubscribeInEachFetchRequest + ":\", " +
-                "\"plaintextChannel\":\"" + plaintextChannel + ":\", " +
-                "\"replayPreset\":\"" + replayPreset + ":\", " +
-                "\"replayId\":\"" + replayId + ":\"}";
+        return "\"SubscriberConfiguration\":{\"topics\":[" + topics + "]";
     }
 
 }
--- src/main/java/nyc/millman/salesforce/processor/SubscriberFactory.java
+++ src/main/java/nyc/millman/salesforce/processor/SubscriberFactory.java
@@ -8,6 +8,8 @@ import nyc.millman.salesforce.external.client.SalesforceClient;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static nyc.millman.salesforce.processor.PubSubService.logger;
 import static nyc.millman.salesforce.processor.PubSubService.printStatusRuntimeException;
@@ -17,26 +19,21 @@ public class SubscriberFactory {
     private final PubSubConfiguration configuration;
     private final SalesforceClient client;
     private final SubscriberConfiguration subscriberConfiguration;
+    private final ExecutorService service;
 
     public SubscriberFactory(PubSubConfiguration configuration, SubscriberConfiguration subscriberConfiguration, SalesforceClient client) {
         this.configuration = configuration;
         this.client = client;
         this.subscriberConfiguration = subscriberConfiguration;
+        this.service = Executors.newFixedThreadPool(subscriberConfiguration.getTopics().size());
     }
 
     public void registerResources() {
         Set<Topic> topics = subscriberConfiguration.getTopics();
-        logger.info("SubscriberFactory.registerResources() topics: " + topics);
         topics.forEach(topic -> {
-            try {
-                logger.info("Registering Subscriber for Topic: " + topic.getPath());
-                var subscriber = new Subscriber(configuration, client, topic.getPath());
-                subscriber.startSubscription();
-            } catch (Exception e) {
-                printStatusRuntimeException("Error during Register Resource", e);
-            }
+            logger.info("Registering Subscriber for Topic: " + topic.getPath());
+            service.submit(new SubscriberCallable(configuration, client, topic.getPath()));
         });
-        logger.info("SubscriberFactory.registerResources() completed");
     }
 
 }
