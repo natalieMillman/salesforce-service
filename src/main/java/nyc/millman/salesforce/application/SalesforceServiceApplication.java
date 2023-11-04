d1dac2c5f308572815b68137cbababede632613f
natalieMillman <natalie@millman.com>
2023-11-04 15:26:04 -0400

    Application can now subscribe to multiple platform events. They are configurable in the dropwizard.yaml file

--- src/main/java/nyc/millman/salesforce/application/SalesforceServiceApplication.java
+++ src/main/java/nyc/millman/salesforce/application/SalesforceServiceApplication.java
@@ -3,16 +3,14 @@ package nyc.millman.salesforce.application;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Environment;
 import nyc.millman.salesforce.external.client.SalesforceClient;
-import nyc.millman.salesforce.processor.Subscriber;
+import nyc.millman.salesforce.processor.SubscriberFactory;
 import nyc.millman.salesforce.service.SalesforceContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static nyc.millman.salesforce.processor.PubSubService.printStatusRuntimeException;
-
 public class SalesforceServiceApplication extends Application<SalesforceServiceConfiguration> {
 
-    private static final Logger logger = LoggerFactory.getLogger(SalesforceServiceApplication.class);
+    private final Logger logger = LoggerFactory.getLogger(SalesforceServiceApplication.class);
 
     public static void main(String[] args) throws Exception {
         new SalesforceServiceApplication().run(args);
@@ -24,13 +22,9 @@ public class SalesforceServiceApplication extends Application<SalesforceServiceC
         SalesforceClient salesforceClient = new SalesforceClient(configuration.getSalesforceClientConfiguration(), configuration.getSalesforceAuthenticationConfiguration());
         SalesforceContext salesforceContext = new SalesforceContext(salesforceClient);
         SalesforceServiceResourceFactory resourceFactory = new SalesforceServiceResourceFactory(environment, salesforceContext).registerResources();
+        SubscriberFactory subscriberFactory = new SubscriberFactory(configuration.getPubSubConfiguration(), configuration.getSubscriberConfiguration(), salesforceClient);
+        subscriberFactory.registerResources();
 
-        try {
-            Subscriber subscribe = new Subscriber(configuration.getSubscriberConfiguration(), salesforceClient);
-            subscribe.startSubscription();
-        } catch (Exception e) {
-            printStatusRuntimeException("Error during Subscribe", e);
-        }
     }
 
 
